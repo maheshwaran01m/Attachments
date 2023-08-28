f38634bd0c5839e2420b9ce1c3ce8e94b67bb443
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-08-28 19:20:42 +0530

    Added Library Path

--- Attachments/MainResources/AttachmentsApp.swift
+++ Attachments/MainResources/AttachmentsApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct AttachmentsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      let _ = print("Path: \(URL.documentsDirectory.path())")
+      ContentView()
     }
+  }
 }
